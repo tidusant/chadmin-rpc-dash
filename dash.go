33903b967f89c24167b58f58c3258e415c56c782
duyhph <duyhph@gmail.com>
2018-01-16 08:04:54 +0700

    add

---
+++ dash.go
@@ -0,0 +1,105 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"net"
+
+	"github.com/tidusant/c3m-common/c3mcommon"
+	"github.com/tidusant/c3m-common/log"
+	"github.com/tidusant/chadmin-repo/models"
+
+	"net/rpc"
+	"strconv"
+	"strings"
+
+	rpch "github.com/tidusant/chadmin-repo/cuahang"
+)
+
+const (
+	defaultcampaigncode string = "XVsdAZGVmY"
+)
+
+type Arith int
+
+func (t *Arith) Run(data string, result *string) error {
+	log.Debugf("Call RPCdash args:" + data)
+	*result = ""
+	//parse args
+	args := strings.Split(data, "|")
+
+	if len(args) < 3 {
+		return nil
+	}
+	var usex models.UserSession
+	usex.Session = args[0]
+	usex.Action = args[2]
+	info := strings.Split(args[1], "[+]")
+	usex.UserID = info[0]
+	usex.ShopID = info[1]
+	usex.Params = ""
+	if len(args) > 3 {
+		usex.Params = args[3]
+	}
+
+	if usex.Action == "getshop" {
+		shopid := rpch.GetShopDefault(usex.UserID)
+
+		if shopid == "" {
+			*result = c3mcommon.ReturnJsonMessage("0", "shop not found", "", "")
+		} else {
+			rpch.LoadShopById(usex.Session, usex.UserID, usex.ShopID)
+			*result = c3mcommon.ReturnJsonMessage("1", "", "shop found", "")
+		}
+	} else if usex.Action == "submitorder" {
+		//*result = submitorder(siteid, mongoSession, data2)
+	} else { //default
+		*result = ""
+	}
+
+	return nil
+}
+
+func main() {
+	var port int
+	var debug bool
+	flag.IntVar(&port, "port", 9878, "help message for flagname")
+	flag.BoolVar(&debug, "debug", false, "Indicates if debug messages should be printed in log files")
+	flag.Parse()
+
+	logLevel := log.DebugLevel
+	if !debug {
+		logLevel = log.InfoLevel
+
+	}
+
+	log.SetOutputFile(fmt.Sprintf("adminDash-"+strconv.Itoa(port)), logLevel)
+	defer log.CloseOutputFile()
+	log.RedirectStdOut()
+
+	//init db
+	arith := new(Arith)
+	rpc.Register(arith)
+	log.Infof("running with port:" + strconv.Itoa(port))
+
+	//			rpc.HandleHTTP()
+	//			l, e := net.Listen("tcp", ":"+strconv.Itoa(port))
+	//			if e != nil {
+	//				log.Debug("listen error:", e)
+	//			}
+	//			http.Serve(l, nil)
+
+	tcpAddr, err := net.ResolveTCPAddr("tcp", ":"+strconv.Itoa(port))
+	c3mcommon.CheckError("rpc dail:", err)
+
+	listener, err := net.ListenTCP("tcp", tcpAddr)
+	c3mcommon.CheckError("rpc init listen", err)
+
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			continue
+		}
+		go rpc.ServeConn(conn)
+	}
+}
